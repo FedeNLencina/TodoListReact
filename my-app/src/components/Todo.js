a46d88a35e6ece35c8ace634d8700f648a1a4872
Federico Lencina <federiconahuellencina@gmail.com>
2021-09-11 17:30:48 -0300

    second commit

---
+++ my-app/src/components/Todo.js
@@ -0,0 +1,19 @@
+function Todo(props){
+    function deleteHandler(){
+        
+    }
+
+    return(
+      <div>     
+        <div className='card'>
+            <h2>{props.text}</h2>
+            <div className='actions'>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
+            </div>
+        </div>
+      </div>
+
+    )
+}
+
+export default Todo;
